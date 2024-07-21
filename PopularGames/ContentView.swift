fab5815fd67f638b69cd0fb3b079b8b69689d473
Zuhrul Anam <zuhrrlanam@gmail.com>
2024-07-21 12:44:09 +0700

    Initial Commit

---
+++ PopularGames/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PopularGames
+//
+//  Created by WDT on 21/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
